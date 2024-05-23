ab78dcd1cc6f3a39396ca67e47f2fc057e7ff149
asimaranov <a.simaranov@gmail.com>
2024-05-23 17:10:54 +0300

    Fixes for shim connection

--- packages/sdk/src/transaction/AuroSigner.ts
+++ packages/sdk/src/transaction/AuroSigner.ts
@@ -10,10 +10,18 @@ export class AuroSigner extends AppChainModule<unknown> implements Signer {
   public async sign(message: Field[]): Promise<Signature> {
     // eslint-disable-next-line max-len
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions,@typescript-eslint/no-unsafe-assignment
-    const response = await (window as any).mina.signFields({
-      message: message.map((field) => field.toString()),
-    });
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    return Signature.fromBase58(response.signature);
+    try {
+      const response = await (window as any).mina.signFields({
+        message: message.map((field) => field.toString()),
+      });
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      return Signature.fromBase58(response.signature);
+    } catch (e: any) {
+      if (e.code == 1001) {
+        await (window as any).mina.requestAccounts();
+        return await this.sign(message);
+      }
+      throw e;
+    }
   }
 }
